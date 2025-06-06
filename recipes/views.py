4a458097ca920a3f7b666ce66b6a0b142f060660
Mateus Silva <mateusilva767@gmail.com>
2025-06-06 18:05:01 -0300

    created page details recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Mateus Silva'
+    })

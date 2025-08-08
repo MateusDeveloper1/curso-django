78817efbe59262eb0f79ab4185f569c21a2a43d2
Mateus Silva <mateusilva767@gmail.com>
2025-08-08 18:31:01 -0300

    validation forms

---
+++ authors/forms.py
@@ -0,0 +1,103 @@
+from django import forms
+from django.forms import ValidationError
+from django.contrib.auth.models import User
+import re
+
+def add_attr(field, attr_name, attr_new_value):
+    existing_attr = field.widget.attrs.get(attr_name, "")
+    field.widget.attrs[attr_name] = f"{existing_attr} {attr_new_value}".strip()
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, "placeholder", placeholder_val)
+
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if regex.match(password):
+        raise ValidationError((
+            "Paasword must have at least one uppercase letter",
+            "one lowercase letter and one number. The legnth should be",
+            "at least 8 characters"
+        ),
+            code="invalid"
+        )
+
+
+class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields["username"], "Your username")
+        add_placeholder(self.fields["email"], "Your e-mail")
+        add_placeholder(self.fields["first_name"], "Ex.: Joe")
+        add_placeholder(self.fields["last_name"], "Ex.: Doe")
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            "placeholder": "Your password"
+        }),
+        error_messages={
+            "required": "Password must not be empty"
+        },
+        help_text=(
+            "Paasword must have at least one uppercase letter",
+            "one lowercase letter and one number. The legnth should be",
+            "at least 8 characters"
+        ),
+        validators=[strong_password]
+    )
+    password2=forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            "placeholder": "Reapet your password"
+        })
+    )
+
+    
+
+
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password"
+        ]
+
+        labels = {
+            "username": "Username",
+            "first_name": "First name",
+            "last_name": "Last name",
+            "email": "E-mail",
+            "password": "Password"
+        }
+        help_texts = {
+            "email": "The e-mail must be valid."
+        }
+        error_messages = {
+            "username": {
+                "required": "This field must not be empty",
+            }
+        }
+        widgets = {
+            "first_name": forms.TextInput(attrs={
+                "placeholder": "Type your username here",
+            }),
+            "password": forms.PasswordInput(attrs={
+                "placeholder": "Type your password here"
+            })
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get("password")
+        password2 = cleaned_data.get("password2")
+
+        if password != password2:
+            raise ValidationError({
+                "password": "Password and password2 must be equal"
+            })

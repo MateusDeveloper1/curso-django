d0918d3250270777f66dacfa0685cc0cc27c9834
Mateus Silva <mateusilva767@gmail.com>
2025-08-13 17:17:30 -0300

    refactoring form

--- authors/forms.py
+++ authors/forms.py
@@ -1,103 +1,122 @@
-from django import forms
-from django.forms import ValidationError
-from django.contrib.auth.models import User
 import re
 
-def add_attr(field, attr_name, attr_new_value):
-    existing_attr = field.widget.attrs.get(attr_name, "")
-    field.widget.attrs[attr_name] = f"{existing_attr} {attr_new_value}".strip()
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
 
 def add_placeholder(field, placeholder_val):
-    add_attr(field, "placeholder", placeholder_val)
+    add_attr(field, 'placeholder', placeholder_val)
 
 
 def strong_password(password):
     regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
 
-    if regex.match(password):
+    if not regex.match(password):
         raise ValidationError((
-            "Paasword must have at least one uppercase letter",
-            "one lowercase letter and one number. The legnth should be",
-            "at least 8 characters"
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
         ),
-            code="invalid"
+            code='invalid'
         )
 
 
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        add_placeholder(self.fields["username"], "Your username")
-        add_placeholder(self.fields["email"], "Your e-mail")
-        add_placeholder(self.fields["first_name"], "Ex.: Joe")
-        add_placeholder(self.fields["last_name"], "Ex.: Doe")
+        add_placeholder(self.fields['username'], 'Your username')
+        add_placeholder(self.fields['email'], 'Your e-mail')
+        add_placeholder(self.fields['first_name'], 'Ex.: John')
+        add_placeholder(self.fields['last_name'], 'Ex.: Doe')
+        add_placeholder(self.fields['password'], 'Type your password')
+        add_placeholder(self.fields['password2'], 'Reapeat your password')
 
-    password = forms.CharField(
-        required=True,
-        widget=forms.PasswordInput(attrs={
-            "placeholder": "Your password"
-        }),
+    username = forms.CharField(
+        label="Username",
+        help_text=(
+            "Username must have letters, numbers or one of those @.+-_. "
+            "The length should be between 4 and 150 characters."
+        ),
         error_messages={
-            "required": "Password must not be empty"
+            'required': 'This field must not be empty',
+            "min_length": "Username must have at least 4 characters",
+            "max_length": "Username must have less than 150 characters"
+        },
+        min_length=4, max_length=150
+    )
+    first_name = forms.CharField(
+        error_messages={"required": "Write your first name"},
+        label="First name"
+    )
+    last_name = forms.CharField(
+        error_messages={"required": "Write your last name"},
+        label="Last name"
+    )
+    email = forms.EmailField(
+        error_messages={"required": "E-mail is required"},
+        label="E-mail",
+        help_text="The e-mail must be valid."
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(),
+        error_messages={
+            'required': 'Password must not be empty'
         },
         help_text=(
-            "Paasword must have at least one uppercase letter",
-            "one lowercase letter and one number. The legnth should be",
-            "at least 8 characters"
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
         ),
-        validators=[strong_password]
+        validators=[strong_password],
+        label="Password"
     )
-    password2=forms.CharField(
-        required=True,
-        widget=forms.PasswordInput(attrs={
-            "placeholder": "Reapet your password"
-        })
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(),
+        label="Password2",
+        error_messages={
+            'required': 'Please, repeat your password'
+        },
     )
-
-    
-
 
     class Meta:
         model = User
         fields = [
-            "first_name",
-            "last_name",
-            "username",
-            "email",
-            "password"
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password',
         ]
 
-        labels = {
-            "username": "Username",
-            "first_name": "First name",
-            "last_name": "Last name",
-            "email": "E-mail",
-            "password": "Password"
-        }
-        help_texts = {
-            "email": "The e-mail must be valid."
-        }
-        error_messages = {
-            "username": {
-                "required": "This field must not be empty",
-            }
-        }
-        widgets = {
-            "first_name": forms.TextInput(attrs={
-                "placeholder": "Type your username here",
-            }),
-            "password": forms.PasswordInput(attrs={
-                "placeholder": "Type your password here"
-            })
-        }
+    def clean_email(self):
+        email = self.cleaned_data.get("email", "")
+        exist = User.objects.filter(email=email).exists
+
+        if exist:
+            raise ValidationError("User email is already in use", code="invalid")
+        
+        return email
 
     def clean(self):
         cleaned_data = super().clean()
 
-        password = cleaned_data.get("password")
-        password2 = cleaned_data.get("password2")
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
 
         if password != password2:
+            password_confirmation_error = ValidationError(
+                'Password and password2 must be equal',
+                code='invalid'
+            )
             raise ValidationError({
-                "password": "Password and password2 must be equal"
+                'password': password_confirmation_error,
+                'password2': [
+                    password_confirmation_error,
+                ],
             })

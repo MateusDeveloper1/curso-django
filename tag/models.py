61d4d255e26ded02d2599dbd024229ad894d5874
Mateus Silva <mateusilva767@gmail.com>
2025-10-03 16:06:27 -0300

    change tag model

--- tag/models.py
+++ tag/models.py
@@ -1,5 +1,3 @@
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.text import slugify
 import string
@@ -8,9 +6,7 @@ from random import SystemRandom
 class Tag(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.CharField(max_length=255)
-    content_object = GenericForeignKey('content_type', 'object_id')
+    
 
     def save(self, *args, **kwargs):
         if not self.slug:

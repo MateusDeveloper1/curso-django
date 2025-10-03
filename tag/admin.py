826ee44dcd74a51039871fb243ed9a4edf73e179
Mateus Silva <mateusilva767@gmail.com>
2025-10-03 16:05:18 -0300

    add list per page and autocomplete fields

--- tag/admin.py
+++ tag/admin.py
@@ -6,7 +6,7 @@ class TagAdmin(admin.ModelAdmin):
     list_display = 'id', 'name', 'slug',
     list_display_links = 'id', 'slug',
     search_fields = 'id', 'slug', 'name',
-    list_per_page = 10,
+    list_per_page = 10
     list_editable = 'name',
     ordering = '-id',
     prepopulated_fields = {
